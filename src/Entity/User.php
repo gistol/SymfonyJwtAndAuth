83588b84542f5bdbd109c5ef7ce2154666092680
Sergey.local <seladon80@gmail.com>
2019-10-20 22:12:58 +0300

    getInfo

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -128,7 +128,7 @@ class User implements UserInterface
     private $vkPhone;
 
     /**
-     * @ORM\OneToOne(targetEntity="App\Entity\Image", mappedBy="imageFile")
+     * @ORM\OneToOne(targetEntity="App\Entity\Image", mappedBy="id")
      */
     private $image;
 
