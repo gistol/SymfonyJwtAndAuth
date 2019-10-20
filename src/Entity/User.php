e363de6bcaefcd56aa80096272519f2f7fdb8f95
Sergey.local <seladon80@gmail.com>
2019-10-20 16:58:28 +0300

    getInfo

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -440,6 +440,7 @@ class User implements UserInterface
     {
         $this->devices = new ArrayCollection();
         $this->notifications = new ArrayCollection();
+        $this->image = new ArrayCollection();
     }
 
     public function getDevices()
