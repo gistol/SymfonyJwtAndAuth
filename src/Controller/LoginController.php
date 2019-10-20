70ee8a01c620cdfcb8246067251a4cac3d2e6843
srudnev <srudnev@synergy.ru>
2019-10-20 09:43:14 +0000

    user_info_vk

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -44,33 +44,47 @@ class LoginController extends AbstractFOSRestController
         $content = $response->toArray();
         $accessToken = $content['access_token'];
         $userId = $content['user_id'];
+        $email = $content['email'];
 
         $vkApiVersion = $this->getParameter('vk.api.version');
         $cl = HttpClient::create();
-        $res = $cl->request('GET', "https://api.vk.com/method/users.get?user_ids=$userId&fields=photo_max_orig,city,contacts&v=$vkApiVersion");
+        $res = $cl->request('GET', "https://api.vk.com/method/users.get?access_token=$accessToken&user_ids=$userId&fields=photo_max_orig,city,contacts&v=$vkApiVersion");
         $con = $res->toArray();
 
-        echo('<pre>');print_r($con);echo('</pre>');
-        echo('<pre>');print_r($content);echo('</pre>');die;
+        //[id] => 13637121
+        //[first_name] => Сергей
+        //[last_name] => Руднев
+        //[city] => Array
+        //(
+        //  [id] => 1
+        //  [title] => Москва
+        //)
+        //
+        //[photo_max_orig] => https://sun9-41.userapi.com/c852216/v852216494/152c82/faLhaKNAGHM.jpg?ava=1
+        //[home_phone] =>
 
-        $user = new User();
-        $user->setPassword('qwertyuiop')
-        ->setVkToken($accessToken)
-        ->setVkId($userId)
-        ->setEmail('ru@ru.ru')
-        ->setFirstName( $con['first_name'])
-        ->setLastName( $con['last_name'])
-        ->setVkPhone( $con['mobile_phone'])
-        ->setRoles(['ROLE_USER']);
+        if(count($con['response'][0]) > 0){
+            $fields = $con['response'][0];
+            $user = new User();
+            $user->setPassword('qwertyuiop')
+                ->setVkToken($accessToken)
+                ->setVkId($userId)
+                ->setEmail($email)
+                ->setFirstName( $fields['first_name'])
+                ->setLastName( $fields['last_name'])
+                ->setVkPhone( $fields['mobile_phone'])
+                ->setRoles(['ROLE_USER']);
+            
+            $em = $this->getDoctrine()->getEntityManager();
+            $em->persist($user);
+            $em->flush();
 
-        $em = $this->getDoctrine()->getEntityManager();
-        $em->persist($user);
-        $em->flush();
-
-        $jwt = $JWTManager->create($user);
-        $appLink = $this->getParameter('app_link').$jwt;
-        echo('<pre>');print_r($appLink);echo('</pre>');die;
-        return $this->redirect($appLink, 301);
+            $jwt = $JWTManager->create($user);
+            $appLink = $this->getParameter('app_link').$jwt;
+            echo('<pre>');print_r($appLink);echo('</pre>');die;
+            return $this->redirect($appLink, 301);
+        }
+        
     }
 
 }
